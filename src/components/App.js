f7b527358a4544961d2804c63bf07f812ed9e25a
DanielAguas <dan.aagsc@gmail.com>
2024-04-17 02:33:22 -0600

    Se agrego Custom Hook useHTTP

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
 import TituloMenu from './TituloMenu';
 import Cart from './Cart';
 import MenuContext from '../context/MenuContext';
+import useHttp from '../hooks/use-http';
 
 function App() {
   const [allProducts, setAllProducts] = useState([]);
   const [total, setTotal] = useState(0);
   const [countProducts, setCountProducts] = useState(0);
   const [basketIconAnimation, setBasketIconAnimation] = useState(false);
+  const { isLoading, error, request } = useHttp();
+
+  
+  const fetchMenu = async () => {
+    try {
+      const data = await request({ url: 'https://react-tacos-default-rtdb.firebaseio.com/' });
+      
+      console.log(data);
+    } catch (error) {
+      console.error('Error al obtener el menú:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
   return (
     <div className='App'>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react"; // Agrega useEffect a la importación
 import Platillo from "./Platillo";
 import Card from "./Card";
 import "../styles/menu.css";
 import MenuContext from "../context/MenuContext";
+import useHttp from '../hooks/use-http'; // Importa el hook useHttp
 
 function Menu() {
   const {
@@ -10,45 +11,24 @@ function Menu() {
     setAllProducts,
     setBasketIconAnimation
   } = useContext(MenuContext);
+  const { isLoading, error, request } = useHttp(); // Usa el hook useHttp
+  const [menu, setMenu] = useState([]);
 
-  const menu = [
-    {
-      id: 1,
-      tipo: "Pastor",
-      descripcion: "Carne de cerdo marinada en especias como achiote, chile y ajo.",
-      precio: 18
-    },
-    {
-      id: 2,
-      tipo: "Suadero",
-      descripcion: "Corte de res procedente de la región abdominal del animal, cocido lentamente hasta alcanzar una textura tierna y jugosa.",
-      precio: 18.00
-    },
-    {
-      id: 3,
-      tipo: "Longaniza",
-      descripcion: "Embutido de carne de cerdo sazonado con especias como ajo, pimienta y orégano, cocinado a la plancha.",
-      precio: 15.00
-    },
-    {
-      id: 4,
-      tipo: "Bistec",
-      descripcion: "Trozos de carne de bistec, típicamente de res, marinados y asados a la parrilla.",
-      precio: 15.00
-    },
-    {
-      id: 5,
-      tipo: "Campechano",
-      descripcion: "Ofrece una mezcla sabrosa de dos tipos de carne, típicamente bistec y chorizo, combinados para crear una explosión de sabores.",
-      precio: 16.00
-    },
-    {
-      id: 6,
-      tipo: "Tripa",
-      descripcion: "Trozos crujientes de tripa de res, fritos hasta alcanzar una textura crujiente por fuera y tierna por dentro.",
-      precio: 25.00
+  // Función para obtener el menú desde la base de datos
+  const fetchMenu = async () => {
+    try {
+      const data = await request({ url: 'URL_DEL_MENÚ' }); // Reemplaza 'URL_DEL_MENÚ' con la URL correcta
+      // Aquí puedes procesar los datos recibidos y establecer el menú
+      console.log(data);
+    } catch (error) {
+      console.error('Error al obtener el menú:', error);
     }
-  ];
+  };
+
+  // Llama a fetchMenu cuando se monta el componente
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
   const onAddProduct = (platillo) => {
     setAllProducts([...allProducts, platillo]);
